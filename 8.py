ed8293039f37ef92c3745b9c4e81572aa8e707e7
kuredo <72093165+prodkuredo@users.noreply.github.com>
2021-06-22 20:06:05 +0300

    Create 8.py

---
+++ 8.py
@@ -0,0 +1,8 @@
+a = ['м', 'а', 'т', 'в', 'е', 'й']
+count = 0
+g = list(itertools.permutations(a,r=6)
+                 #.product(a, r=6), если не нужно, чтобы буквы повторялись только один раз
+for i in g:
+    if ''.join(i)[0] != 'й' and ''.join(i).count('ае') == 0:
+        count += 1
+print(count)
